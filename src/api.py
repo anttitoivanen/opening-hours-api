29d468a122a0a62576d151fa030311603e91e679
Antti Toivanen <atoivane@gmail.com>
2019-02-13 14:21:04 +0200

    Make response types more uniform and sensible

--- src/api.py
+++ src/api.py
@@ -4,14 +4,18 @@ from .parser import Parser
 app = Flask(__name__)
 parser = Parser()
 
+response_headers = {'Content-Type': 'text/plain'}
+
 @app.route('/parse_opening', methods=['POST'])
 def parse_opening():
 	if not request.data or not request.json:
-		return make_response('Error: Missing or empty request body', 400)
+		message = 'Error: Missing or empty request body'
+		return make_response(message, 400, response_headers)
 	try:
 		data = parser.validate_input(request.json)
 	except ValueError as e:
-		return make_response(f'Error: {str(e)}', 400)
+		message = f'Error: {str(e)}'
+		return make_response(message , 400, response_headers)
 	parsed = parser.parse_input(data)
-	out = parser.format_output(parsed)
-	return out
+	result = parser.format_output(parsed)
+	return make_response(result, 200, response_headers)
