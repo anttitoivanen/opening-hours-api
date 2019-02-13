91c41afbd1715650cd24aa39cef478cbc9a8348f
Antti Toivanen <atoivane@gmail.com>
2019-02-13 16:31:35 +0200

    Make handling the response types a little cleaner

--- src/api.py
+++ src/api.py
@@ -1,21 +1,35 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response
 from .parser import Parser
 
 app = Flask(__name__)
 parser = Parser()
 
-response_headers = {'Content-Type': 'text/plain'}
+# Set up a default class for responses that always has the type text/plain
+# We want to be consistent ith response types, but not to set them every time
+class PlainTextResponse(app.response_class):
+	def __init__(self, *args, **kwargs):
+		super(PlainTextResponse, self).__init__(*args, **kwargs)
+		self.mimetype = 'text/plain'
+app.response_class = PlainTextResponse
 
 @app.route('/parse_opening', methods=['POST'])
 def parse_opening():
 	if not request.data or not request.json:
 		message = 'Error: Missing or empty request body'
-		return make_response(message, 400, response_headers)
+		return make_response(message, 400)
 	try:
 		data = parser.validate_input(request.json)
 	except ValueError as e:
 		message = f'Error: {str(e)}'
-		return make_response(message , 400, response_headers)
+		return make_response(message , 400)
 	parsed = parser.parse_input(data)
 	result = parser.format_output(parsed)
-	return make_response(result, 200, response_headers)
+	return make_response(result, 200)
+
+@app.errorhandler(405)
+def method_not_alowed(error):
+	return make_response('Method not allowed')
+
+@app.errorhandler(500)
+def internal_error(error):
+	return make_response('There was in internal error', 500)
