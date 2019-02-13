59120b5b3f1b640e60623e82882d812b6d273b0b
Antti Toivanen <atoivane@gmail.com>
2019-02-13 14:12:19 +0200

    Make api error handling a bit more uniform

--- src/api.py
+++ src/api.py
@@ -6,13 +6,12 @@ parser = Parser()
 
 @app.route('/parse_opening', methods=['POST'])
 def parse_opening():
-	if not request.json:
-		resp = make_response(jsonify({'error': 'Missing request body'}), 400)
+	if not request.data or not request.json:
+		return make_response('Error: Missing or empty request body', 400)
 	try:
 		data = parser.validate_input(request.json)
 	except ValueError as e:
-		resp = make_response(jsonify({'error': str(e)}), 400)
-		return resp
+		return make_response(f'Error: {str(e)}', 400)
 	parsed = parser.parse_input(data)
 	out = parser.format_output(parsed)
 	return out
